86d47472918dbd169ba3ca3ad350529858d1ee07
Qdust41 <Qdust41@gmail.com>
2025-04-16 19:32:59 -0400

    Some edits made to the files; uploading for cloud storage

--- SDcard_CSV_Data/SDcard_CSV_Data.ino
+++ SDcard_CSV_Data/SDcard_CSV_Data.ino
@@ -17,30 +17,34 @@ void setup() {
   // Open or create the file
   dbFile = SD.open("data.csv", FILE_WRITE);
   if (dbFile) {
-    // Write a header if needed
-    dbFile.println("timestamp,sensor1,sensor2");
-    // Write a row of data
-    dbFile.print(millis());
-    dbFile.print(",");
-    dbFile.print(analogRead(A0));
-    dbFile.print(",");
-    dbFile.println(analogRead(A1));
-
-    Serial.println("Data written to SD card.");
+    // Write a header
+    dbFile.println("timestamp,A0,A1,A2,A3,A4,A5");
+    Serial.println("Collumns written to SD card. Please wait until data is wrote");
   } else {
     Serial.println("Failed to open file for writing.");
+    return;
   }
 }
 
 void loop() {
   // Yes i made a loop in a loop that runs once; no i dont care.
-  for (int i = 0; i <= 100; i++) {
+  for (int i = 0; i < 100; i++) {
     dbFile.print(millis());
     dbFile.print(",");
     dbFile.print(analogRead(A0));
     dbFile.print(",");
-    dbFile.println(analogRead(A1)); // Using a println makes a new line after so we can iterate through each time and just append a row every time (for CSV file types)
+    dbFile.print(analogRead(A1));
+    dbFile.print(",");
+    dbFile.print(analogRead(A2));
+    dbFile.print(",");
+    dbFile.print(analogRead(A3));
+    dbFile.print(",");
+    dbFile.print(analogRead(A4));
+    dbFile.print(",");
+    dbFile.println(analogRead(A5)); 
+    // Using a println makes a new line after so we can iterate through each time and just append a row every time (for CSV file types)
   }
   dbFile.close();
+  Serial.println("Data written to SD card; safe to remove");
   return; // just to close the loop bc i dont want to test it a shitton just want to see if this works tbh  
 }
