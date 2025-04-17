aca338a639ed7977882b9ce9d7d9c9f687b2f6ea
Qdust41 <Qdust41@gmail.com>
2025-04-16 22:37:36 -0400

    Did some more edits to code stuff

--- SDcard_CSV_Data/SDcard_CSV_Data.ino
+++ SDcard_CSV_Data/SDcard_CSV_Data.ino
@@ -1,35 +1,17 @@
 #include <SD.h>
 #include <SPI.h>
+#include <Eventually.h>
 
 const int chipSelect = 10; // This is the MISO pin on the SD card thing and which pin it connects to on arduino
+
+EvtManager mgr(true); // true to manage memory
 
 // Sets up dbFile as our 'global' variable for our SD card file
 File dbFile;
 
-void setup() {
-  Serial.begin(115200);
-  if (!SD.begin(chipSelect)) {
-    Serial.println("SD card initialization failed!");
-    return;
-  }
-  Serial.println("SD card initialized.");
-
-  // Open or create the file
-  dbFile = SD.open("data.csv", FILE_WRITE);
+bool addrow() {
   if (dbFile) {
-    // Write a header
-    dbFile.println("timestamp,A0,A1,A2,A3,A4,A5");
-    Serial.println("Collumns written to SD card. Please wait until data is wrote");
-  } else {
-    Serial.println("Failed to open file for writing.");
-    return;
-  }
-}
-
-void loop() {
-  // Yes i made a loop in a loop that runs once; no i dont care.
-  for (int i = 0; i < 100; i++) {
-    dbFile.print(millis());
+    dbFile.print(millis() - 240);
     dbFile.print(",");
     dbFile.print(analogRead(A0));
     dbFile.print(",");
@@ -42,9 +24,51 @@ void loop() {
     dbFile.print(analogRead(A4));
     dbFile.print(",");
     dbFile.println(analogRead(A5)); 
-    // Using a println makes a new line after so we can iterate through each time and just append a row every time (for CSV file types)
+  } else {
+    Serial.println("Issue adding row to file");
   }
-  dbFile.close();
-  Serial.println("Data written to SD card; safe to remove");
-  return; // just to close the loop bc i dont want to test it a shitton just want to see if this works tbh  
+  return true;
+}
+
+void setup() {
+  // Setup for the SD card
+  Serial.begin(115200);
+  if (!SD.begin(chipSelect)) {
+    Serial.println("SD card initialization failed!");
+    return;
+  }
+  Serial.println("SD card initialized.");
+
+  // Open or create the file
+  dbFile = SD.open("data.csv", FILE_WRITE);
+  if (dbFile) {
+    // Write a header
+    dbFile.println("timestamp,A0,A1,A2,A3,A4,A5");
+    Serial.println("Columns written to SD card. Please wait until data is written");
+  } else {
+    Serial.println("Failed to open file for writing.");
+    return;
+  }
+
+  // Setup for the event handler
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+
+  mgr.addListener(new EvtTimeListener(500, true, (EvtAction)addrow));
+}
+
+void loop() {
+  
+  mgr.loopIteration();
+
+  if ((millis() - 240) > 10000) {
+    dbFile.close();
+    Serial.println("Data written to SD card; safe to remove");
+    while (1) {}; // PLEASE JUST FUCKING STOP THE LOOP I HATE IT HERE
+  }
+
 }
