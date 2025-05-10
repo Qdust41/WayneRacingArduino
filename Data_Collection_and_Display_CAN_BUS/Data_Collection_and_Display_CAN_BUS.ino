952cb48949de9fe90e3cfefc7505d027ba2909dc
Qdust41 <qdust41@gmail.com>
2025-05-10 19:06:02 -0400

    Edited logic and moved around variable declerations

--- Data_Collection_and_Display_CAN_BUS/Data_Collection_and_Display_CAN_BUS.ino
+++ Data_Collection_and_Display_CAN_BUS/Data_Collection_and_Display_CAN_BUS.ino
@@ -2,7 +2,6 @@
 #include <SPI.h>
 #include <Eventually.h>
 #include <mcp2515.h>
-#include <mcp2515_defs.h>
 #include <ThermocoupleCounter.h>
 #include <SoftwareSerial.h>
 #include <genieArduino.h>
@@ -26,7 +25,16 @@ bool sensorFailureState = false;
 unsigned long lastBlinkTime = 0;
 const int blinkInterval = 500; // 500ms blink rate
 int data = 0;
-int oilPressure =0;
+
+// CAN msg data (might need to adjust the variables scopes)
+int rpm = 0;
+int gaugeValue = 0;
+int oilPressure = 0;
+int oilTemp = 0;
+int fuelPressure = 0;
+int coolantTempIn = 0;
+int coolantTempOut = 0;
+int gearPosition = 0;
 
 ThermocoupleCounter tempSensor(3, 4, 5, 11); //clkPin, csPin, doPin, counterClkPin
 
@@ -41,20 +49,17 @@ EvtManager mgr(true); // true to manage memory
 // Sets up dbFile as our 'global' variable for our SD card file
 File dbFile;
 
-void displayUpdate(){
-
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
-    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
-      data += int(canMsg.data[i]);
-    }
-    // Decode RPM
-    if (canMsg.can_id == 0x123) {  // Replace with actual EL129 CAN ID for RPM
-      int rpm = data/canMsg.can_dlc;
+// All of the values for the CAN id's need to be properly setup to their actual values
+void decodeCan() {
+  switch (canMsg.can_id) {
+    case (0x123):
+      // Decode RPM
+      rpm = data/canMsg.can_dlc;
       Serial.print("RPM: "); Serial.println(rpm);
-      genie.WriteObject(GENIE_OBJ_LED_DIGITS, 0, rpm);  // Update RPM display
+      genie.WriteObject(GENIE_OBJ_LED_DIGITS, 0, rpm);
 
-      // Update RPM Gauge
-      int gaugeValue = (rpm / 1000) * TICKS_PER_1000_RPM;
+      // Adjust the rpm gauge
+      gaugeValue = (rpm / 1000) * TICKS_PER_1000_RPM;
       genie.WriteObject(GENIE_OBJ_GAUGE, 0, gaugeValue);
 
       // Change gauge color based on RPM
@@ -70,53 +75,59 @@ void displayUpdate(){
       } else {
         shiftAlertState = false;
       }
-    }
-
-    // Decode Oil Temp
-    if (canMsg.can_id == 0x456) {  
-      int oilTemp = data/canMsg.can_dlc;
+      break;
+    case (0x456):
+      // Decode Oil Temperatures
       Serial.print("Oil Temp: "); Serial.println(oilTemp);
       genie.WriteObject(GENIE_OBJ_LED_DIGITS, 1, oilTemp);
-    }
-
-    // Decode Oil Pressure
-    if (canMsg.can_id == 0x789) {  
+      break;
+    case (0x789):
+      // Decode Oil Pressures
       oilPressure = data/canMsg.can_dlc;
       Serial.print("Oil Pressure: "); Serial.println(oilPressure);
-      genie.WriteObject(GENIE_OBJ_LED_DIGITS, 2, oilPressure);
-    }
-
-    // Decode Fuel Pressure
-    if (canMsg.can_id == 0xABC) {  
-      int fuelPressure = data/canMsg.can_dlc;
+      genie.WriteObject(GENIE_OBJ_LED_DIGITS, 1, oilPressure);
+      break;
+    case (0xABC):
+      // Decode Fuel Pressures
+      fuelPressure = data/canMsg.can_dlc;
       Serial.print("Fuel Pressure: "); Serial.println(fuelPressure);
       genie.WriteObject(GENIE_OBJ_LED_DIGITS, 3, fuelPressure);
-    }
-
-    // Decode Coolant Temp In (°C)
-    if (canMsg.can_id == 0xDEF) {  
-      int coolantTempIn = data/canMsg.can_dlc;
+      break;
+    case (0xDEF):
+      // Decode Coolant Temp In (°C)
+      coolantTempIn = data/canMsg.can_dlc;
       Serial.print("Coolant Temp In (°C): "); Serial.println(coolantTempIn);
       genie.WriteObject(GENIE_OBJ_LED_DIGITS, 4, coolantTempIn);
-    }
-
-    // Decode Coolant Temp Out (°C)
-    if (canMsg.can_id == 0x101) {  
-      int coolantTempOut = data/canMsg.can_dlc;
+      break;
+    case (0x101):
+      // Decode Coolant Temp Out (°C)
+      coolantTempOut = data/canMsg.can_dlc;
       Serial.print("Coolant Temp Out (°C): "); Serial.println(coolantTempOut);
       genie.WriteObject(GENIE_OBJ_LED_DIGITS, 5, coolantTempOut);
-    }
-
-    // Decode Gear Position (1-digit display)
-    if (canMsg.can_id == 0x202) {  
-      int gearPosition = data/canMsg.can_dlc;
+      break;
+    case (0x202):
+      // Decode Gear Position (1-digit display)
+      gearPosition = data/canMsg.can_dlc;
       Serial.print("Gear Position: "); Serial.println(gearPosition);
       genie.WriteObject(GENIE_OBJ_LED_DIGITS, 6, gearPosition);
+      break;
+    default:
+      Serial.print("Error receiving CAN message or decoding it");
+      break;
+    }
+}
+
+void displayUpdate(){
+
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) {
+    for (int i = 0; i<canMsg.can_dlc; i++)  {  // print the data
+      data += int(canMsg.data[i]);
     }
 
+    decodeCan();
 
     // Sensor failure detection (example: if oil pressure reads 255, consider it an error)
-    if (oilPressure == 255) {  
+    if (oilPressure >= 255) {  
       sensorFailureState = true;
     } else {
       sensorFailureState = false;
@@ -130,16 +141,19 @@ void displayUpdate(){
   delay(100);
 }
 
+unsigned long currentTime = millis();
+static bool shiftLedState = false;
+static bool sensorLedState = false;
 // Function to handle flashing LED indicators
 void handleFlashingAlerts() {
-  unsigned long currentTime = millis();
+  currentTime = millis();
 
   if (currentTime - lastBlinkTime >= blinkInterval) {
     lastBlinkTime = currentTime;
 
     // Flash Shift Alert LED (Primitive Circle 1)
     if (shiftAlertState) {
-      static bool shiftLedState = false;
+      shiftLedState = false;
       shiftLedState = !shiftLedState;
       genie.WriteObject(GENIE_OBJ_USER_LED, 1, shiftLedState);
     } else {
@@ -148,7 +162,7 @@ void handleFlashingAlerts() {
 
     // Flash Sensor Failure Alert LED (Primitive Circle 2)
     if (sensorFailureState) {
-      static bool sensorLedState = false;
+      sensorLedState = false;
       sensorLedState = !sensorLedState;
       genie.WriteObject(GENIE_OBJ_USER_LED, 2, sensorLedState);
     } else {
@@ -157,6 +171,8 @@ void handleFlashingAlerts() {
   }
 }
 
+int temp = 0;
+int iternal = 0;
 // Slightly messy code to add rows to the CSV file
 bool addrow() {
   if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) { // reads CAN messages
@@ -179,8 +195,8 @@ bool addrow() {
   }
   
   //live updates the temp of thermocoupler
-  int temp = 0;
-  int iternal = 0;
+  temp = 0;
+  iternal = 0;
   temp, iternal = tempSensor.update();
   
   if (dbFile) { //prints rows
@@ -219,6 +235,7 @@ bool addrow() {
     dbFile.print(id2_data);
     dbFile.print(",");
 
+    // Additional Testing stuff
     dbFile.print(analogRead(A0));
     dbFile.print(",");
     dbFile.print(analogRead(A1));
@@ -272,6 +289,7 @@ bool addrow() {
   } else {
     // This should be unrechable but just in case for logging to the serial output
     Serial.println("Issue adding row to file");
+    return false;
   }
   return true;
 }
@@ -287,23 +305,20 @@ void setup() {
   genie.Begin(DisplaySerial);
   genie.WriteContrast(15);
   Serial.begin(500000);
-  if (!SD.begin(chipSelect)) {
+  if (SD.begin(chipSelect)) {
+    // Open or create the file
+    generateFilename();
+    dbFile = SD.open(filename, FILE_WRITE);
+    if (dbFile) {
+      // Write the CSV header lines
+      dbFile.println("timestamp,RR Damper,RL Damper,FR Damper,FL Damper,Steering,A5,A6,A7,A8,A9,A10,A11,A12,A13,ID1,ID2,F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12,2F1,2F2,2F3,2F4,2F5,2F6,2F7,2F8,2F9,2F10,2F11,2F12");
+      Serial.println("Headers written to SD card.\nPlease wait until data is written");
+  } } else {
     Serial.println("SD card initialization failed!");
-    while (1) {}; // Stops if there is no SD card
+    // Removed blocking because this code is also used for the display which will not need an SD card
+    // while (1) {}; // Stops if there is no SD card
   }
   Serial.println("SD card initialized.");
-
-  // Open or create the file
-  generateFilename();
-  dbFile = SD.open(filename, FILE_WRITE);
-  if (dbFile) {
-    // Write the CSV header lines
-    dbFile.println("timestamp,RR Damper,RL Damper,FR Damper,FL Damper,Steering,A5,A6,A7,A8,A9,A10,A11,A12,A13,ID1,ID2,F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12,2F1,2F2,2F3,2F4,2F5,2F6,2F7,2F8,2F9,2F10,2F11,2F12");
-    Serial.println("Headers written to SD card.\nPlease wait until data is written");
-  } else {
-    Serial.println("Failed to open file for writing.");
-    while (1) {}; //Should be unrechable but just in case again
-  }
 
   // Setup for the event handler (this is in case we opt for some event based system instead)
   pinMode(A0, INPUT);
@@ -338,11 +353,12 @@ void loop() {
   if (millis() > 30000) { // We will need to determine a better way to know how long to collect data for or customize it on a per test basis
     dbFile.close();
     Serial.println("Data written to SD card; safe to remove");
-    while (1) {}; // PLEASE JUST STOP THE LOOP I HATE IT HERE 
+    while (1) {}; // Stops the loop
   }
 
 }
 
+// To make new files per test
 void generateFilename() {
   int fileNumber = 0;
   bool fileExists = true;
